36df3ad25143ef366389afe4cb4719e23b7452fb
Opiwe <jnkqubela@gmail.com>
2024-07-05 13:11:08 +0200

    added delete for database in node

--- server/server.js
+++ server/server.js
@@ -104,10 +104,17 @@ app.put('/api/v1/restaurants/:id', async (req, res) => {
 });
 
 // Delete a restaurant
-app.delete('/api/v1/restaurants/:id', (req, res) => {
-    res.status(204).json({
-        status: 'success',
-    });
+app.delete('/api/v1/restaurants/:id', async (req, res) => {
+
+    try {
+        const results = await db.query("DELETE FROM restaurants where id = $1", [req.params.id]);
+        
+        res.status(204).json({
+            status: 'success',
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.listen(PORT, () => {
