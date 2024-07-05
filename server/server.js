72281f62cafc2be2670f758da28c3c92828ab724
Opiwe <jnkqubela@gmail.com>
2024-07-05 09:49:16 +0200

    added one single id and create it for database in node

--- server/server.js
+++ server/server.js
@@ -37,27 +37,47 @@ app.get('/api/v1/restaurants', async (req, res) => {
 });
 
 // Get one single restaurant
-app.get('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.params);
+app.get('/api/v1/restaurants/:id', async (req, res) => {
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            restaurant: "mcdonlanlds"
-        }
-    });
+    console.log(req.params.id);
+
+    try {
+        const results = await db.query("select * from restaurants where id = $1", [req.params.id,]);
+        // select * from restaurants where id = req.params.id
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                restaurant: results.rows[0],
+            }
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // Create a restaurants
-app.post('/api/v1/restaurants', (req, res) => {
+app.post('/api/v1/restaurants', async (req, res) => {
+    
     console.log(req.body);
 
-    res.status(201).json({
-        status: 'success',
-        data: {
-            restaurant: "mcdonlanlds"
-        }
-    });
+    try {
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", 
+        [req.body.name, req.body.location, req.body.price_range]);
+
+        console.log(results);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                restaurant: results.rows[0],
+            }
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // Update a restaurant
